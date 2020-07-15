bc8f9427b8e94fed409e182ace5b644124b9a929
RobertDalyCode <RobertDalyCode@noreply-github.com>
2020-07-15 21:29:00 +0100

    Add project files.

---
+++ MathsMania/UserGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace MathsMania
+{
+    using System;
+	using System.Collections.Generic;
+    /// <summary>
+    /// User Group to assign roles and permissions
+    /// </summary>
+	public interface UserGroup
+    {
+        Int32 Id { get; set; }
+        String Name { get; set; }
+        IList<UserAccount> Users { get; set; }
+    }
+}
